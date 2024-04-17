53b1abf9037701c9b224d773583731c4e4ed3ef5
kolomaa <sherbachenkoliza@gmail.com>
2024-04-17 09:24:19 +0300

    add changes

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const { GoogleGenerativeAI } = require('@google/generative-ai');
-
 const genAI = new GoogleGenerativeAI(process.env.APY_KEY);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post('/data', urlencodedParser, (req, res) => {
-  const result = run(req.body.question);
+app.post('/data', urlencodedParser, async (req, res) => {
+  const result = await run(req.body.question);
   res.send(result);
 });
 
 async function run(prompt) {
-  // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
-
-  // const prompt = 'Write a story about a magic backpack.';
-
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
@@ -34,8 +31,6 @@ async function run(prompt) {
   return text;
 }
 
-// run();
-
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server has been started on port ${port}...`);
 });
